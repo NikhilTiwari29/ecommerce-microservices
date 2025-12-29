cc1eb5ee873e37641c4a3f41c9a4a1bea4ca0c3f
NikhilTiwari29 <nikhiltiwarip29@gmail.com>
2025-12-30 02:05:32 +0530

    added mock wire for feign client api call mocking in integration test

--- order-service/src/test/java/com/nikhil/microservices/order/configs/BaseIntegrationTestConfiguration.java
+++ order-service/src/test/java/com/nikhil/microservices/order/configs/BaseIntegrationTestConfiguration.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.mysql.MySQLContainer;
+import org.wiremock.spring.EnableWireMock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableWireMock
 public abstract class BaseIntegrationTestConfiguration {
 
     @ServiceConnection
--- order-service/src/test/java/com/nikhil/microservices/order/integration/OrderIntegrationTest.java
+++ order-service/src/test/java/com/nikhil/microservices/order/integration/OrderIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.nikhil.microservices.order.integration;
 
 import com.nikhil.microservices.order.configs.BaseIntegrationTestConfiguration;
+import com.nikhil.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
@@ -22,6 +23,8 @@ public class OrderIntegrationTest extends BaseIntegrationTestConfiguration {
                       "quantity" : 10
                   }
                 """;
+
+        InventoryClientStub.stubInventoryCall("iphone 15",10);
 
         RestAssured.given()
                 .contentType(ContentType.JSON)
