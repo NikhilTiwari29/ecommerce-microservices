fb392e6bffbb2f6b13c24d702ac0673f2dc7c625
NikhilTiwari29 <nikhiltiwarip29@gmail.com>
2026-01-17 01:53:03 +0530

    moved code from feign to rest client

--- order-service/src/main/java/com/nikhil/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/nikhil/microservices/order/client/InventoryClient.java
@@ -5,8 +5,6 @@ import com.nikhil.microservices.order.advices.ApiResponse;
 import groovy.util.logging.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
@@ -14,38 +12,36 @@ import java.time.OffsetDateTime;
 
 // Feign client is deprecated
 
-@FeignClient(name = "inventory", url = "${inventory.service.url}")
-public interface InventoryClient {
-
-    @GetMapping("/api/inventory")
-    ApiResponse<Boolean> isInStock(@RequestParam String skuCode,
-                                   @RequestParam Integer quantity);
-}
+//@FeignClient(name = "inventory", url = "${inventory.url}")
+//public interface InventoryClient {
+//
+//    @GetMapping("/api/inventory")
+//    ApiResponse<Boolean> isInStock(@RequestParam String skuCode,
+//                                   @RequestParam Integer quantity);
+//}
 
 // HTTP Interface / Rest Client mechanism is the new approach.
 
-//@Slf4j
-//public interface InventoryClient {
-//
-//    Logger log = LoggerFactory.getLogger(InventoryClient.class);
-//
-//    @GetExchange("/api/inventory")
-//    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-//    @Retry(name = "inventory")
-//    ApiResponse<Boolean> isInStock(@RequestParam String skuCode,
-//                                   @RequestParam Integer quantity);
-//
-//    default ApiResponse<Boolean> fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
-//        log.warn("Inventory fallback triggered for skuCode={}, quantity={}, reason={}",
-//                skuCode, quantity, throwable.getMessage());
-//
-//        return new ApiResponse<>(
-//                OffsetDateTime.now().toString(),
-//                503,
-//                "/api/inventory",
-//                null, // important: not false
-//                new ApiErrorResponse("Inventory service unavailable", null)
-//        );
-//    }
-//
-//}
+@Slf4j
+public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
+    @GetExchange("/api/inventory")
+    ApiResponse<Boolean> isInStock(@RequestParam String skuCode,
+                                   @RequestParam Integer quantity);
+
+    default ApiResponse<Boolean> fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.warn("Inventory fallback triggered for skuCode={}, quantity={}, reason={}",
+                skuCode, quantity, throwable.getMessage());
+
+        return new ApiResponse<>(
+                OffsetDateTime.now().toString(),
+                503,
+                "/api/inventory",
+                null, // important: not false
+                new ApiErrorResponse("Inventory service unavailable", null)
+        );
+    }
+
+}
---
+++ order-service/src/main/java/com/nikhil/microservices/order/config/RestClientConfig.java
@@ -0,0 +1,48 @@
+package com.nikhil.microservices.order.config;
+
+import com.nikhil.microservices.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .requestFactory(jdkRequestFactory())
+                .build();
+
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+
+        HttpServiceProxyFactory factory =
+                HttpServiceProxyFactory.builderFor(adapter).build();
+
+        return factory.createClient(InventoryClient.class);
+    }
+
+    private JdkClientHttpRequestFactory jdkRequestFactory() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .build();
+
+        JdkClientHttpRequestFactory factory =
+                new JdkClientHttpRequestFactory(httpClient);
+
+        factory.setReadTimeout(Duration.ofSeconds(3));
+        return factory;
+    }
+}
